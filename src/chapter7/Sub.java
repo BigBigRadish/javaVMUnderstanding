9963b7bcc31652ebe02a890255b48967a195973e
萝卜 <superradish@outlook.com>
2018-06-01 13:12:28 +0800

    class loading

---
+++ src/chapter7/Sub.java
@@ -0,0 +1,16 @@
+import chapter7;
+static class Parent {
+		public static int A = 1;
+		static {
+			A = 2;
+		}
+}
+
+static class Sub extends Parent {
+		public static int B = A;
+}
+
+public static void main(String[] args) {
+		System.out.println(Sub.B);
+}
+
